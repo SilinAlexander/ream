3d1a795d75c7e6d80e074907b62506c0fb7a6661
SilinAlexander <asilin1997@mail.ru>
2021-02-27 09:16:23 +0500

    2

--- ream/web/main/views.py
+++ ream/web/main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.views.generic.base import View
 
-# Create your views here.
+from .models import *
+
+
+class BaseView(View):
+
+    def get(self, request, *args, **kwargs):
+        news = LatestNews.objects.get_news_for_main_page('news')
+        context = {
+            'news': news,
+        }
+        return render(request, 'base.html', context)
--- ream/web/src/settings.py
+++ ream/web/src/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = ')@loqcvy63=29bg2ux1xyyjy8+aws^jehd65%i#jxp9(bhblr5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
+import os
 
 # Application definition
 
@@ -119,3 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static_dev'),
+
+)
